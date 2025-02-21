9cf0113d7504b5ce1b8def2e1096a13bd1a5ccac
leartaademi <learta.ademi@bwz-rappi.ch>
2025-02-21 15:15:01 +0100

    controller

--- MenuProjekt/src/Controller/DishController.java
+++ MenuProjekt/src/Controller/DishController.java
@@ -1,6 +1,35 @@
 package Controller;
 
+import java.util.List;
+
 public class DishController {
 
+    private Model.DishModel dishModel;
+
+    public DishController() {
+        dishModel = new Model.DishModel();
+        dishModel.loadDishes();
+    }
+
+    public void addDish(Model.DishModel dish) {
+        dishModel.addDish(dish);
+    }
+
+    public void updateDish(int index, Model.DishModel dish) {
+        dishModel.updateDish(index, dish);
+    }
+
+    public Model.Dish getDish(int index){
+        return dishModel.getDish(index);
+    }
+
+    public void deleteDish(int index) {
+        dishModel.deleteDish(index);
+    }
+
+    public List<Model.Dish> getDishes() {
+        return dishModel.getDishes();
+    }
+    
 
 }
