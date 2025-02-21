8fdcc2748a4a4d0f65308d8904c8dcf968a8f672
leartaademi <learta.ademi@bwz-rappi.ch>
2025-02-21 11:43:24 +0100

    dish and dishmodel

--- MenuProjekt/src/model/Dish.java
+++ MenuProjekt/src/model/Dish.java
@@ -1,2 +1,22 @@
-package model;public class Dish {
+package model;
+
+public class Dish {
+
+    String dishname;
+    String ingredients;
+    Category category;
+    String price;
+    DietaryRestrictions dietaryRestrictions;
+    int spicinessRating;
+
+    public Dish(String dishname, String ingredients, Category category, String price, DietaryRestrictions dietaryRestrictions, int spicinessRating) {
+        this.dishname = dishname;
+        this.ingredients = ingredients;
+        this.category = category;
+        this.price = price;
+        this.dietaryRestrictions = dietaryRestrictions;
+        this.spicinessRating = spicinessRating;
+    }
+
+
 }
--- MenuProjekt/src/model/DishModel.java
+++ MenuProjekt/src/model/DishModel.java
@@ -1,2 +1,64 @@
-package model;public class DishModel {
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DishModel {
+    List<Dish> dishes;
+
+    public DishModel() {
+        dishes = new ArrayList<Dish>();
+    }
+
+    public List<Dish> getDefaultDishes() {
+
+        List<Dish> defaultDishes = new ArrayList<>();
+
+        Dish pizza = new Dish(
+                "Pizza Margherita",
+                "Tomaten, Mozzarella, Basilikum",
+                Category.MAINDISH,
+                "9.99",
+                DietaryRestrictions.VEGETARIAN,
+                1
+        );
+
+        Dish sushi = new Dish(
+                "Sushi Set",
+                "Reis, Lachs, Algen, Sojasauce",
+                Category.MAINDISH,
+                "14.99",
+                DietaryRestrictions.GLUTENFREE,
+                2
+        );
+
+        Dish brownie = new Dish(
+                "Schoko Brownie",
+                "Schokolade, Mehl, Zucker",
+                Category.DESSERT,
+                "4.99",
+                DietaryRestrictions.VEGETARIAN,
+                0
+        );
+
+        Dish bruschetta = new Dish(
+                "Bruschetta",
+                "Geröstetes Brot, Tomaten, Knoblauch, Basilikum, Olivenöl",
+                Category.APPETIZER,
+                "5.49",
+                DietaryRestrictions.VEGAN,
+                0
+        );
+
+        List<Dish> menu = List.of(pizza, sushi, brownie, bruschetta);
+
+        defaultDishes.add(pizza);
+        defaultDishes.add(sushi);
+        defaultDishes.add(brownie);
+        defaultDishes.add(bruschetta);
+
+        return getDefaultDishes();
+
+
+    }
 }
